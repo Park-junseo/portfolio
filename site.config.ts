3a306106beb8242e28e4ff8b847e23a9eb45902f
Park-junseo <psbsanno@gmail.com>
2024-11-22 01:51:32 +0900

    modify site config

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'dcc33a7b393e4b22bf5c37ea0ee510c8',
+  rootNotionPageId: '145f8efc41568090aa00c46927826478',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
