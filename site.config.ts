283203a2c01bd3c1c475edbcc3c43bfc7f427326
Park-junseo <psbsanno@gmail.com>
2024-11-23 22:00:00 +0900

    Scroll FadeInUp

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '145f8efc41568090aa00c46927826478',
+  rootNotionPageId: process.env.NEXT_PUBLIC_NOTION_PAGE_ID!,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Park-Junseo\'s Portfolio',
-  domain: 'park-junseo.vercel.app',
+  domain: process.env.NEXT_PUBLIC_DOMAIN!,
   author: 'Junseo Park',
 
   // open graph metadata (optional)
